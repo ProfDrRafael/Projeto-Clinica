2af5cd131d6129609dd2523abc4a83a747ead187
MottinDev <qwertyukj@gmail.com>
2025-06-06 00:08:04 -0400

    Bugfix: altera o nome em tableColums, de plantão para tipo_atendimento

--- src/main/java/Visao/Telas/TableListaAtendimento.java
+++ src/main/java/Visao/Telas/TableListaAtendimento.java
@@ -20,9 +20,11 @@ public class TableListaAtendimento extends SimpleForm {
     public TableListaAtendimento() {
         initComponents();
 
-        String[] tableColumns = new String[]{"#", "ID", "Data", "Hora", "Preenchido", "Comparecimento", "Plantão", "Prontuário", "Estagiário"};
-        String queryTable = "SELECT id, data, hora, preenchido, comparecimento, plantao, prontuario_id, estagiario_id FROM atendimento";
+        String[] tableColumns = new String[]{"#", "ID", "Data", "Hora", "Preenchido", "Comparecimento", "Tipo", "Prontuário", "Estagiário"};
+        String queryTable = "SELECT id, data, hora, preenchido, comparecimento, tipo_atendimento, prontuario_id, estagiario_id FROM atendimento";
         boolean acao_ativar_ou_inativar = false;
+
+
 
 
         CreateCustomTable customTable = new CreateCustomTable(queryTable, tableColumns, "Atendimentos", "Atendimento", acao_ativar_ou_inativar, "Inativar", "/Multimidia/imagens/cadeado.png");
